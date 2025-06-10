fe1585ff7d096066a7b53e4e9ad2fa03f6a48dd0
Mirjam <mirjam.mclachlan@gmail.com>
2025-06-10 13:44:07 +0200

    fixed?

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,20 +2,20 @@ var modal = document.getElementById("imageModal");
 var modalImg = document.getElementById("modalImage");
 var closeBtn = document.getElementsByClassName("close")[0];
 
-// Add click event to all images inside the #main div
-document.querySelectorAll("#main img").forEach(function (img) {
+// Add click event to all images with the clickable-image class
+document.querySelectorAll(".clickable-image").forEach(function (img) {
     img.addEventListener("click", function () {
         modal.style.display = "block";
         modalImg.src = this.src;
     });
 });
 
-// Close the modal when the X is clicked
+// Close modal when X is clicked
 closeBtn.onclick = function () {
     modal.style.display = "none";
 }
 
-// Close the modal when anywhere outside the image is clicked
+// Close modal when clicking outside the image
 modal.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
